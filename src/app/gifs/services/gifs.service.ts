77ed7513d0aa03e82afdc840c5b605b924138678
SebasEsp1818 <sebasesgu@gmail.com>
2026-08-17 14:50:59 -0600

    Historial y Caché de búsqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mappper/gif.mapper';
-import { map, pipe } from 'rxjs';
+import { map, tap } from 'rxjs';
+
+// Record<string, Gif[]>
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
@@ -14,6 +16,9 @@ export class GifService {
 
   trendingGifs = signal<Gif[]>([]);
   trendingGfsLoanding = signal(true);
+
+  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
   constructor() {
     this.loadTrendingGifs();
@@ -38,6 +43,7 @@ export class GifService {
 
 
 
+  
   searchGifs(query: string) {
     return this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
@@ -49,8 +55,19 @@ export class GifService {
       })
       .pipe(
         map(({ data }) => data),
-        map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+
+        // TODO: Historial
+        tap(items => {
+          this.searchHistory.update(history => ({
+            ...history,
+            [query.toLowerCase()]: items,
+          }))
+        })
       );
+    // .subscribe((resp) => {
+    //   const gifs = GifMapper.mapGiphyItemsTGifArray(resp.data);
+    //   console.log({ search: gifs });
+    // });
   }
 }
-
