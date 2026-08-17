d3bb4e22cbb128118d55b61b9905a748f48d3d91
SebasEsp1818 <sebasesgu@gmail.com>
2026-08-17 13:38:08 -0600

    Mostrar GIFs en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -2,18 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
-import { Gif } from '../../interfaces/gif.interface';
-import { GifMapper } from '../mapper/gif.mapper';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mappper/gif.mapper';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private http = inject(HttpClient);
 
-  // Único signal activo en este paso: los GIFs en tendencia
   trendingGifs = signal<Gif[]>([]);
+  trendingGfsLoanding = signal(true);
 
   constructor() {
     this.loadTrendingGifs();
+    console.log('Servicio creado')
   }
 
   loadTrendingGifs() {
@@ -27,6 +28,7 @@ export class GifsService {
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsTGifArray(resp.data);
         this.trendingGifs.set(gifs);
+        this.trendingGfsLoanding.set(false);
         console.log({ gifs });
       });
   }
